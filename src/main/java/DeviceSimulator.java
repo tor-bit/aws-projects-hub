51fa52843197010328c00638bb099e3f65123894
tor-bit <thorscira@gmail.com>
2025-07-12 19:51:46 +0200

    initial commit

---
+++ src/main/java/DeviceSimulator.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DeviceSimulator {
+
+    private static Map<Integer, Float> lastTemps = new HashMap<>();
+    private static Random rand = new Random();
+
+    private static float generateRandomTemperature(int deviceId) {
+        float lastTemp = lastTemps.getOrDefault(deviceId, 25.0f);
+        float delta = rand.nextFloat() * 2 - 1;
+        float newTemp = lastTemp + delta;
+        lastTemps.put(deviceId, newTemp);
+        return newTemp;
+    }
+
+    public static void simulateDevices(int deviceCount, int eventsPerDevice) {
+        for (int x = 0; x < deviceCount; x++) {
+            for (int y = 0; y < eventsPerDevice; y++) {
+                Device device = new Device(x, generateRandomTemperature(x));
+                System.out.println(device.toJson());
+                try {
+                    Thread.sleep(500); 
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        simulateDevices(5, 10);
+    }
+}
